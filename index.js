95542caf28e815bc8896385e2f0a3a710edb2e4a
serhii <sri@ciklum.com>
2018-03-13 16:28:29 +0200

    app2

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!!')
-})
+});
+
+app.get('/foo', (req, res) => {
+  res.send(`foo: ${req.query[Object.keys(req.query)[0]]}`);
+});
+
+app.get('/bar', (req, res) => {
+  res.send(`bar: ${req.query[Object.keys(req.query)[0]]}`);
+});
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
