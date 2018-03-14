97eaeaaee0192fd792830f0844e91f2657571883
serhii <sri@ciklum.com>
2018-03-14 11:21:17 +0200

    connect to db

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
-
 const app = express();
+const path = require("path");
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const URL = 'mongodb://localhost:27017'
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!!')
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
+
+app.post('/save', (req, res) => {
+  MongoClient.connect(URL, function(err, client) {
+    const collection = client.db('test').collection('names');
+
+    collection.insert({name: req.body.name}, function(err, result) {
+
+      if (err) return;
+
+      client.close();
+      res.redirect('/');
+    })
+  })
+})
 
 app.get('/foo', (req, res) => {
   res.send(`foo: ${req.query[Object.keys(req.query)[0]]}`);
